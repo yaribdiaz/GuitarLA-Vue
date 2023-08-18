0be352b95f4148c80f89a3dfbc2cb17f6dd855a2
Yarib Díaz <yaribdiazz@gmail.com>
2023-08-18 12:08:07 -0600

    build

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+  import { ref, reactive, onMounted, watch } from 'vue';
+  import {db} from './data/guitarras'
+  import Guitarra from './components/Guitarra.vue'
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+
+  const guitarras = ref([])
+  const carrito = ref([])
+  const guitarra = ref({})
+
+  watch(carrito, () => {
+    carritoLocalStorage()
+  }, {
+    deep:true
+  })
+
+  onMounted(() => {
+    guitarras.value = db
+    guitarra.value =  db[3]
+
+    const carritoStorage = localStorage.getItem('carrito')
+    if(carritoStorage) carrito.value = JSON.parse(carritoStorage)
+
+  })
+
+  const carritoLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(carrito.value))
+  }
+
+  const agregarCarrito = (guitarra) => {
+    const existeProducto = carrito.value.findIndex(producto => producto.id === guitarra.id)
+    if(existeProducto >= 0){
+      carrito.value[existeProducto].cantidad++
+      
+    }else{
+      guitarra.cantidad = 1
+      carrito.value.push(guitarra)
+    }
+
+  }
+
+  const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if(carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+  }
+
+  const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if(carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+  }
+  
+  const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+  }
+
+  const vaciarCarrito = () => {
+    carrito.value = []
+  }
+
+</script>
+
+<template>
+    <Header
+      :carrito="carrito"
+      :guitarra="guitarra"
+      @agregar-carrito="agregarCarrito"
+      @incrementar-cantidad="incrementarCantidad"
+      @decrementar-cantidad="decrementarCantidad"
+      @eliminar-producto="eliminarProducto"
+      @vaciar-carrito="vaciarCarrito"
+    />
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+          <Guitarra
+            v-for="guitarra in guitarras"
+            :guitarra="guitarra"
+            :key="guitarra.id"
+            @agregar-carrito="agregarCarrito"
+          />
+        </div>
+    </main>
+    <Footer/>
+</template>|  
+
